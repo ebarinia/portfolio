effcbf320008fb4df18b3022b8b003c09c735247
Emmanuel Debion <emmanueldebion@Emmanuels-MBP.broadband>
2023-09-24 21:47:31 +0100

    portfolio v2.2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
   return (
     <header className="header">
         <nav className="navbar navbar-expand-lg justify-content-between">
-        <a href="/"><img className="logo" src={require("../../images/logo.png")} alt="logo"/></a>
+        {/* <img className="logo" src={require("../../images/logo.png")} alt="logo"/> */}
+        <Link className="nav-link text-white" to={"/"}><img className="logo" src={require("../../images/logo.png")} alt="logo"/></Link>
             <div className="navbar-nav">
                 {/* <Link className="nav-link text-white" to={"/"}><h5>Home</h5></Link> */}
                 <Link className="nav-link text-white" to={"/portfolio"}><h5>Portfolio</h5></Link>
